63eb0290fc29f20bc9d1f31f5bae68da360516d2
sunil.suthar <sunil.suthar@quantumcodexai.com>
2024-08-23 12:12:02 +0530

    javascript in variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 1402;
+let accountEmail = "sunilsuthar@mailinator.com";
+var accountPassword = "sunil123";
+accountCity = "Jodhpur";
+let accountState;
+
+// accountId=2002 //not allowed
+accountEmail = "sunil@mailinator.com";
+accountPassword = "sunil12345";
+accountCity = "Jaipur";
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issues in bloke scopr and functional scopr
+*/
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
