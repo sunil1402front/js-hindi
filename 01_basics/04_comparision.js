35ebe2144352d08402ca79977b99c3afc96ebcfc
sunil.suthar <sunil.suthar@quantumcodexai.com>
2024-08-28 11:35:02 +0530

    javascript comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+console.log(8 > 9); // false
+console.log(8 < 9); // true
+console.log(8 >= 9); // false
+console.log(8 <= 9); // true
+
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined > 0); //false
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
+
+console.log("2" === 2); // its check a === is data type // false
