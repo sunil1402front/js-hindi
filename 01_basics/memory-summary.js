1a239294a8b1ca69d82ce582a237a20ad68172f6
sunil.suthar <sunil.suthar@quantumcodexai.com>
2024-08-28 12:54:03 +0530

    Stack ( Primitive ) , Heap ( Non-Primitive )

---
+++ 01_basics/memory-summary.js
@@ -0,0 +1,21 @@
+// Stack ( Primitive ) , Heap ( Non-Primitive )
+
+let myYotubeName = "SunilSuthar@Youtube.com";
+let anotherName = myYotubeName;
+
+console.log(anotherName);
+
+// Heap ( Non-Primitive )
+
+let userOne = {
+  name: "SunilSuthar",
+  email: "user@gmail.com",
+  upi: "user@ybl",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
