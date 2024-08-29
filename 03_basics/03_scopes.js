22cce5c073e126965b057d8f37954c0048068d15
sunil.suthar <sunil.suthar@quantumcodexai.com>
2024-08-29 16:06:57 +0530

    3_basics/03_scopes.js

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,12 @@
+// code run in terminal this command : node 03_basics/03_scopes.js
+
+var c = 300;
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
